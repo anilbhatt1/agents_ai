a6b8e262b7c46dbda58813df706e8898c5e27fd8
anilbhatt1 <anilaknb@gmail.com>
2024-08-11 21:07:16 +0530

    Modified agents and tasks

--- wom/src/agents.py
+++ wom/src/agents.py
@@ -1,4 +1,6 @@
 import os
+import json
+import yaml
 from textwrap import dedent
 from crewai import Agent
 from tools.browser_tools import BrowserTools
@@ -10,7 +12,7 @@ from langchain_openai import ChatOpenAI
 class PostRankingAgents:
     def __init__(self):
         self.api_file_path = 'api_keys.json'
-        with open(json_file_path, 'r') as file:
+        with open(self.api_file_path, 'r') as file:
             self.api_keys = json.load(file)        
         self.openai_gpt35 = ChatOpenAI(model_name="gpt-3.5-turbo", 
                                        temperature=0.7,
@@ -20,25 +22,70 @@ class PostRankingAgents:
                                       openai_api_key=self.api_keys['openai'])        
         self.llama31_8b = Ollama(model='llama3.1:8b',
                                  openai_api_key=self.api_keys['ollama'])
+        self.gemma2_9b = Ollama(model='gemma2:9b',
+                                 openai_api_key=self.api_keys['ollama'])   
+        self.cfg_file_path = 'casaai_config.yaml'
+
+        with open(self.cfg_file_path, 'r') as yaml_file:
+            self.cfg = yaml.safe_load(yaml_file)
+        self.product_long = self.cfg.get('product_long_description', '')      
+        self.product_short = self.cfg.get('product_short_description', '')     
     
-    def agent_1_name(self):
+    def content_analysis_agent(self):
+        backstory = "You are a content analyst with expertise in analyzing web content and \
+                     extracting relevant information. You are responsible for ensuring that \
+                     content is relevant, high-quality, and aligned with the marketing of \
+                    {self.product_short}. "
         return Agent(
-            role="Define agent 1 role here",
-            backstory=dedent(f"""Define agent 1 backstory here"""),
-            goal=dedent(f"""Define agent 1 goal here"""),
-            # tools=[tool_1, tool_2],
+            role="Content Analyst",
+            goal="Analyze web content and extract relevant information",
+            backstory=backstory,
             allow_delegation=False,
             verbose=True,
-            llm=self.OpenAIGPT35,
+            llm=self.llama31_8b,
+        )
+        
+    def engagement_analysis_agent(self):
+        backstory = "You evaluates content by analyzing user interactions, such as likes, \
+                    shares, comments, and views. You should also consider factors such as \
+                    user behavior/sentiment."
+        goal = "Accurately assess the impact and effectiveness of content based on user interactions"
+        return Agent(
+            role="Engagement Analyst",
+            goal=goal,
+            backstory=backstory,
+            allow_delegation=False,
+            verbose=True,
+            llm=self.llama31_8b,
         )
 
-    def agent_2_name(self):
+    def relevance_analysis_agent(self):
+        backstory = "You evaluates content by analyzing its alignment with {self.product_short}. \
+                    You should also consider factors such as keyword density, context \
+                    accuracy, and user intent. You identifies content that effectively \
+                    meets audience expectations, flags irrelevant material, and \
+                    provides insights to enhance content targeting."
+        goal = "Ensure that content is highly pertinent and aligned with the intended topics and audience needs"
         return Agent(
-            role="Define agent 2 role here",
-            backstory=dedent(f"""Define agent 2 backstory here"""),
-            goal=dedent(f"""Define agent 2 goal here"""),
-            # tools=[tool_1, tool_2],
+            role="Relevance Analyst",
+            goal=goal,
+            backstory=backstory,
             allow_delegation=False,
             verbose=True,
-            llm=self.OpenAIGPT35,
-        )    
+            llm=self.llama31_8b,
+        )
+        
+    def content_review_supervisor_agent(self):
+        backstory = "Responsible for synthesizing and evaluating the combined outputs from the \
+                     Content Analysis, Engagement Analysis, and Relevance Analysis agents. You \
+                     ensures all aspects of content—quality, engagement, and relevance—are \
+                     harmonized and aligns with the marketing of {self.product_short}."
+        goal = "Ensure content is relevant, engaging, and strategically aligned to marketing of product"
+        return Agent(
+            role="Content Review Supervisor",
+            goal=goal,
+            backstory=backstory,
+            allow_delegation=False,
+            verbose=True,
+            llm=self.gemma2_9b,
+        )
--- wom/src/tasks.py
+++ wom/src/tasks.py
@@ -1,37 +1,86 @@
 from crewai import Task
 from textwrap import dedent
+import json
+import yaml
+from pydantic import BaseModel
+
+class ScoreOutput(BaseModel):
+    comment_id: str
+    score: float
+    justification: str
 
 class CustomTasks:
-    def __tip_section(self):
-        return "If you do your BEST WORK, I'll give you a $10,000 commission!"
+    def __init__(self):
+        with open(self.cfg_file_path, 'r') as yaml_file:
+            self.cfg = yaml.safe_load(yaml_file)
+        self.product_long  = self.cfg.get('product_long_description', '')      
+        self.product_short = self.cfg.get('product_short_description', '')
+        self.input_format_descr = "Input format is a python list of dictionaries. There will be \
+                                   a parent post and its associated comments. Parent post has \
+                                   'parent_id' = None. Comments have 'parent_id' = 'comment_id' of \
+                                   parent post"   
+        self.tip_text = "If you do your BEST WORK, I'll give you a $10,000 commission!" 
 
-    def task_1_name(self, agent, var1, var2):
+    def content_analysis_task(self, agent):
+        
+        descr = "Analyze posts and associated comments from {input} to determine their relevance based on \
+        identified keywords and phrases w.r.to the marketing of {self.product_long}. Then provide a score \
+        of 10 for each post and comment and provide a justification for each score. {self.tip_text}"
+        
+        expected_out = "JSON with comment_id, relevance score, and a brief justification (less than 15 words) \
+                        explaining the rationale behind the score for each post and its associated comments.", 
+        
         return Task(
-            description=dedent(
-                f"""
-            Do something as part of task 1
-            
-            {self.__tip_section()}
-    
-            Make sure to use the most recent data as possible.
-    
-            Use this variable: {var1}
-            And also this variable: {var2}
-        """
-            ),
+            description=descr,
+            expected_output=expected_out,
             agent=agent,
+            output_json=ScoreOutput,
         )
 
-    def task_2_name(self, agent):
+    def engagement_analysis_task(self, agent):
+        
+        descr = "Analyze posts and associated comments from {input} to determine their relevance based on \
+        identified keywords and phrases w.r.to the marketing of {self.product_long}. Then provide a score \
+        of 10 for each post and comment and provide a justification for each score. {self.tip_text}"
+        
+        expected_out = "JSON with comment_id, relevance score, and a brief justification (less than 15 words) \
+                        explaining the rationale behind the score for each post and its associated comments.", 
+        
         return Task(
-            description=dedent(
-                f"""
-            Take the input from task 1 and do something with it.
-                                       
-            {self.__tip_section()}
-
-            Make sure to do something else.
-        """
-            ),
+            description=descr,
+            expected_output=expected_out,
             agent=agent,
-        )    
+            output_json=ScoreOutput,
+        )
+    
+    def relevance_analysis_task(self, agent):
+        
+        descr = "Analyze posts and associated comments from {input} to determine their relevance based on \
+        identified keywords and phrases w.r.to the marketing of {self.product_long}. Then provide a score \
+        of 10 for each post and comment and provide a justification for each score. {self.tip_text}"
+        
+        expected_out = "JSON with comment_id, relevance score, and a brief justification (less than 15 words) \
+                        explaining the rationale behind the score for each post and its associated comments.", 
+        
+        return Task(
+            description=descr,
+            expected_output=expected_out,
+            agent=agent,
+            output_json=ScoreOutput,
+        )   
+        
+    def supervisor_review_task(self, agent):
+        
+        descr = "Analyze posts and associated comments from {input} to determine their relevance based on \
+        identified keywords and phrases w.r.to the marketing of {self.product_long}. Then provide a score \
+        of 10 for each post and comment and provide a justification for each score. {self.tip_text}"
+        
+        expected_out = "JSON with comment_id, relevance score, and a brief justification (less than 15 words) \
+                        explaining the rationale behind the score for each post and its associated comments.", 
+        
+        return Task(
+            description=descr,
+            expected_output=expected_out,
+            agent=agent,
+            output_json=ScoreOutput,
+        ) 
